4bc1a2413a4b3e26b6688c446349aa855590cf21
TanishaAgarwal <tanishaaggarwal2502@gmail.com>
2019-08-11 09:58:03 +0530

    movies routers are committed

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const movie = require('../app/api/controllers/movies');
 router.post('/movies', movie.create);
 router.get('/movies', movie.findAll);
 router.get('/movies/:movieId', movie.findOne);
 router.put('/movies/:movieId', movie.update);
 router.delete('/movies/:movieId', movie.delete);
+
 module.exports = router;
